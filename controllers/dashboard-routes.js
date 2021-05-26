6a9dd8303c115dfdbcbee8f9f04bf44e7a5d001e
Aftaab Dobani <1234dobani@gmail.com>
2021-05-25 21:01:04 -0400

    added one router get

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,3 +2,24 @@ const router = require('express').Router();
 const { post , user } = require('../models/');
 const withAuth = require('../utils/auth');
 
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const postData = await post.findAll({
+            where: {
+                userId: req.session.userId,
+            },
+        });
+
+        const Posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('all-posts-admin', {
+            layout: 'dashboard', 
+            posts, 
+        });
+
+    }    catch (err) {
+        res.redirect('login');
+    }
+});
+
+router
