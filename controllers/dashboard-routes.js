3d63adb674d89429aa51d1214c5f17f281daec58
Aftaab Dobani <1234dobani@gmail.com>
2021-05-25 21:15:13 -0400

    fixed error on dashboard route

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -30,25 +30,21 @@ router.get('/new', withAuth, (req, res) => {
 
 router.get('/edit/:id', withAuth, async (req, res) => {
     try {
-        const postData = await post.findByPk(req.params.id);
-
-        if (postData) {
-            const post = await Post.findByPk(req.params.id);
-
-            if (postData) {
-                const post = postData.get({ plain: true });
-
-                res.render('edit-post', {
-                    layout: 'dashboard',
-                    post,
-                });
-            } else {
-                res.status(404).end();
-            }
-        } catch (err) {
-            res.redirect('login');
-        }
-
-        }
+      const postData = await Post.findByPk(req.params.id);
+  
+      if (postData) {
+        const post = postData.get({ plain: true });
+  
+        res.render('edit-post', {
+          layout: 'dashboard',
+          post,
+        });
+      } else {
+        res.status(404).end();
+      }
+    } catch (err) {
+      res.redirect('login');
     }
-})
+  }); 
+
+  module.exports = router; 
