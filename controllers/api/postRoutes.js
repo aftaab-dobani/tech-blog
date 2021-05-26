9b0a86df8598c8cc48cee2f2523ff60a9c8c7ae7
Aftaab Dobani <1234dobani@gmail.com>
2021-05-25 20:28:13 -0400

    edited router delete in post routes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -34,19 +34,18 @@ router.put('/:id', withAuth, async (req,res) => {
 
 router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const postData = await Post.destroy({
+    const [affectedRows] = Post.destroy({
       where: {
         id: req.params.id,
-        user_id: req.session.user_id,
+        
       },
     });
 
-    if (!postData) {
-      res.status(404).json({ message: 'No post found!' });
-      return;
+    if (affectedRows > 0) {
+      res.status(200).end();
+    } else {
+      res.status(404).end();
     }
-
-    res.status(200).json(postData);
   } catch (err) {
     res.status(500).json(err);
   }
