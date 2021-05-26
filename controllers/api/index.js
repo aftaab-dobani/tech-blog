71cd3086bea8117c86645330a1e820264549587b
Aftaab Dobani <1234dobani@gmail.com>
2021-05-25 20:14:31 -0400

    edited index.js api controller

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
+const commentRoutes = require('./comment-route');
 
 router.use('/users', userRoutes);
 router.use('/posts', projectRoutes);
+router.use('/comment', commentRoutes);
 
 module.exports = router;
