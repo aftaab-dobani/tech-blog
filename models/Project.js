ff40335d8bf87b7b688516b21fcc0787bd4d559c
Aftaab Dobani <1234dobani@gmail.com>
2021-05-25 21:46:34 -0400

    finished index and project.js

--- models/Project.js
+++ models/Project.js
@@ -1,5 +1,17 @@
-const { Model, DataTypes } = require('sequelize');
+const { Sequelize, Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+
+class Project extends Model {}
+
+Project.init(
+    {
+        title: DataTypes.STRING,
+        body: DataTypes.STRING
+    },
+    {
+        sequelize
+    }
+);
 
 
 module.exports = Project;
--- models/index.js
+++ models/index.js
@@ -3,3 +3,29 @@ const Project = require('./Project');
 
 
 module.exports = { User, Project };
+const User = require('./User');
+const Comment = require('./comment');
+const Project = require('./Project');
+const { post } = require('../controllers');
+
+Post.belongsTo(User, {
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'postId', 
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
+});
+
+module.exports = {
+    User,
+    Comment,
+    Project
+};
+
