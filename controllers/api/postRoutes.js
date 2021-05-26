d2a623519f4ccc7f76725c21b3f3226b082b8fda
Aftaab Dobani <1234dobani@gmail.com>
2021-05-25 20:25:19 -0400

    added router.put in post route

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -3,17 +3,34 @@ const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
+  const body = req.body;
+
   try {
     const newPost = await Post.create({
-      ...req.body,
-      user_id: req.session.user_id,
-    });
+      ...body, user_id: req.session.userId });
 
-    res.status(200).json(newPost);
+    res.json(newPost);
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
 });
+
+router.put('/:id', withAuth, async (req,res) => {
+  try {
+    const [affectedRows] = await Post.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (affectedRows > 0) {
+      res.status(200).end();
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
 
 router.delete('/:id', withAuth, async (req, res) => {
   try {
